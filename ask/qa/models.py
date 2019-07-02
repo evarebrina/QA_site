c205bf3827e8d7d4b4c17114f356a22dc62ae02b
vadimrebrin <vadimrebrin@yahoo.com>
2019-07-02 19:59:17 +0300

    Fix author

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class QuestionManager(models.Model):
+class QuestionManager(models.Manager):
     def new(self):
 	    return self.order_by('-added_at')
 
@@ -14,10 +14,10 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name='question_like_user', null=True)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
     def __unicode__(self):
         return self.title
     def get_absolute_url(self):
@@ -25,6 +25,6 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
     question = models.OneToOneField(Question, null=True, on_delete=models.SET_NULL)
-    author = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
