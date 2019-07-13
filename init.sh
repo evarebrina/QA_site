045dc2d6796dd8bd88d4527d7290931ddc083bdd
vadimrebrin <vadimrebrin@outlook.com>
2019-07-13 19:48:27 +0300

    Update init and gunicorn configuration files

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -sf ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -sf ~/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo ln -sf ~/web/etc/gunicorn-django.py /etc/gunicorn.d/test-django
 sudo /etc/init.d/mysql start
 sudo /etc/init.d/nginx restart
-#gunicorn -c /etc/gunicorn.d/test hello:wsgi_application
-gunicorn -c /etc/gunicorn.d/test-django ask.wsgi:application
+gunicorn --config /etc/gunicorn.d/test-django --pid ~/web/gunicorn.pid --daemon ask.wsgi:application
