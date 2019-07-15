acd460c8f0815c3e90aa45859781a8926b08b418
vadimrebrin <vadimrebrin@outlook.com>
2019-07-15 20:27:54 +0300

    Change init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf ~/web/etc/gunicorn-django.py /etc/gunicorn.d/test-django
-sudo /etc/init.d/mysql start
 sudo /etc/init.d/nginx restart
 gunicorn --config /etc/gunicorn.d/test-django --pid ~/web/gunicorn.pid --daemon ask.wsgi:application
